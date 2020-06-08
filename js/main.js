c3000a5677b06459089799a599cec4b7b22610fc
simpa17 <giandon.stella@gmail.com>
2020-06-09 06:57:16 +0700

    update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var close = document.querySelector('#close')
 
 openMenu.addEventListener('click', function () {
   navBar.style.transform = 'translateX(0)'
+  var list = document.querySelectorAll('.nar-bar__list')
+  list.forEach((step, i) =>
+    step.classList.add('animate__fadeInRight', 'animate__delay-' + i + 's')
+  )
 })
 
 close.addEventListener('click', function () {
