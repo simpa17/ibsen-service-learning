06911a0ec229544551231d19738994c5043cc8d3
simpa17 <giandon.stella@gmail.com>
2020-06-09 16:33:15 +0700

    update search box

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 var openMenu = document.querySelector('#open-menu')
 var navBar = document.querySelector('.nar-bar')
 var close = document.querySelector('#close')
+var openSearch = document.querySelector('#search')
 
 openMenu.addEventListener('click', function () {
   navBar.style.transform = 'translateX(0)'
   var list = document.querySelectorAll('.nar-bar__list')
-  list.forEach((step, i) =>
-    step.classList.add('animate__fadeInRight', 'animate__delay-' + i + 's')
-  )
+  list.forEach((step, i) => step.classList.add('animate__lightSpeedInRight'))
+  var img = document.querySelector('.nar-bar__img')
+  img.classList.add('animate__rotateInDownLeft')
 })
 
 close.addEventListener('click', function () {
   navBar.style.transform = 'translateX(300px)'
+})
+
+var searchBox = document.querySelector('.search-box')
+var closeSearch = document.querySelector('.search-box__close')
+openSearch.addEventListener('click', function () {
+  searchBox.style.transform = 'translateY(0)'
+  searchBox.classList.add('animate__slideInDown')
+})
+
+closeSearch.addEventListener('click', function () {
+  searchBox.style.transform = 'translateY(-100px)'
 })
