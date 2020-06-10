59995bb5cf053ad0db80d4eaf35c23acf739870a
simpa17 <giandon.stella@gmail.com>
2020-06-10 16:20:55 +0700

    update slider

--- js/main.js
+++ js/main.js
@@ -17,11 +17,54 @@ close.addEventListener('click', function () {
 
 var searchBox = document.querySelector('.search-box')
 var closeSearch = document.querySelector('.search-box__close')
+var headerNavBar = document.querySelector('.header__nav-bar')
 openSearch.addEventListener('click', function () {
-  searchBox.style.transform = 'translateY(0)'
   searchBox.classList.add('animate__slideInDown')
+  searchBox.classList.remove('close')
+  headerNavBar.classList.add('close', 'animate__slideInDown')
 })
 
 closeSearch.addEventListener('click', function () {
-  searchBox.style.transform = 'translateY(-100px)'
+  console.log(searchBox)
+  searchBox.classList.add('close')
+  searchBox.classList.remove('animate__slideInDown')
+  headerNavBar.classList.remove('close')
+})
+
+$(document).ready(function () {
+  var currentImage = 1
+  var totalImages = 4
+  function increaseImage() {
+    ++currentImage
+    if (currentImage > totalImages) {
+      currentImage = 1
+    }
+  }
+  function decreaseImage() {
+    --currentImage
+    if (currentImage < 1) {
+      currentImage = totalImages
+    }
+  }
+  window.setInterval(function () {
+    $('#next').click()
+  }, 3000)
+  $('#next').on('click', function () {
+    $('#img_' + currentImage)
+      .stop()
+      .fadeOut(1)
+    decreaseImage()
+    $('#img_' + currentImage)
+      .stop()
+      .fadeIn(1)
+  })
+  $('#previous').on('click', function () {
+    $('#img_' + currentImage)
+      .stop()
+      .fadeOut(1)
+    increaseImage()
+    $('#img_' + currentImage)
+      .stop()
+      .fadeIn(1)
+  })
 })
